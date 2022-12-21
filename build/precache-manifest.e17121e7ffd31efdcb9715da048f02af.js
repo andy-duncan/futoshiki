e25b3a54bf0e6c4272804db6e706b1ceddb70f06
andy-duncan <github@igiveada.mn>
2022-12-21 15:01:53 +0100

    push built files

---
+++ build/precache-manifest.e17121e7ffd31efdcb9715da048f02af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c395035e4b522ce8ebf899d84e7bd019",
+    "url": "/futoshiki/index.html"
+  },
+  {
+    "revision": "1dc2d5676bf8c8d46bdf",
+    "url": "/futoshiki/static/js/2.f8d729cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/futoshiki/static/js/2.f8d729cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "063a412a18033f9ea3ff",
+    "url": "/futoshiki/static/js/main.760d66ad.chunk.js"
+  },
+  {
+    "revision": "419f7761cf3be6ec334d",
+    "url": "/futoshiki/static/js/runtime-main.1a0ab4e7.js"
+  }
+]);
